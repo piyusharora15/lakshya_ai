1b5359bbd79fdcb20f65011e4ccd74f7b5b8d856
Piyush Arora <piyusharora1504@gmail.com>
2025-04-17 20:36:18 +0530

    make clerk auth dark

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,10 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
+import { Toaster } from "sonner";
 import Header from "@/components/ui/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const inter = Inter({subsets:["latin"]});
 export const metadata = {
@@ -12,7 +14,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+    appearance={{
+      baseTheme: dark,
+    }}>
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className}`}
@@ -26,10 +31,11 @@ export default function RootLayout({ children }) {
             {/* header */}
             <Header />
             <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
             {/* footer */}
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with love by Piyush Arora</p>
+                <p>Your one-stop solution for interview-prep and personalized guidance!</p>
               </div>
             </footer>
           </ThemeProvider>
