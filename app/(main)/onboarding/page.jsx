99e8c5f137ff4b77fea24474f543be5ad1475c87
Piyush Arora <piyusharora1504@gmail.com>
2025-04-19 20:46:07 +0530

    Onboarding Page

--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -1,11 +1,11 @@
+import { industries } from '@/data/industries';
 import React from 'react'
 
 const OnboardingPage = () => {
-  return (
-    <div>
-      OnboardingPage
-    </div>
-  )
-}
+  // Check if User is already onboarded
+  return <main>
+    <OnboardingForm industries={industries}/>
+    </main>;
+};
 
-export default OnboardingPage
+export default OnboardingPage;
