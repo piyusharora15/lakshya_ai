f61f44de6a3e23365609d2801da23ea639143f7e
Piyush Arora <piyusharora1504@gmail.com>
2025-04-19 20:43:50 +0530

    layout of main folder

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const MainLayout = () => {
-  return (
-    <div>
-      MainLayout
-    </div>
-  )
-}
+const MainLayout = ({children}) => {
+// Redirect to Onboarding
+
+  return <div className='container mx-auto mt-24 mb-20'>{children}</div>;
+};
 
 export default MainLayout
